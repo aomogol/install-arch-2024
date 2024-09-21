e451a47abfc89e84f6ed8c24d018d0643d73e523
A Onder Mogol <aomogol@gmail.com>
2024-09-21 09:28:04 +0300

    git sh update

--- 6-git-setup.sh
+++ 6-git-setup.sh
@@ -7,3 +7,10 @@ set -e
 
 git config --global user.name "A Onder Mogol"
 git config --global user.email aomogol@gmail.com
+
+# Kimlik bilgilerini önbelleğe alma
+git config --global credential.helper cache
+
+# İsteğe bağlı: Önbellek süresini ayarlama (örneğin, 1 saat için = 3600)
+git config --global credential.helper 'cache --timeout=36000'
+
